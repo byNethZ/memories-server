bddd671032e6b2e92682274b997c77dfe3d6219a
NethZ <bynethz@gmail.com>
2022-08-08 06:29:41 -0500

    Modify Post Controller, index.js

--- index.js
+++ index.js
@@ -3,19 +3,20 @@ import bodyParser from 'body-parser';
 import mongoose  from 'mongoose';
 import cors  from 'cors';
 import {credentials} from './credentials.js';
+import dotenv from 'dotenv';
 
 import postRoutes from './routes/posts.js';
 
 const app = express();
-
-app.use('/posts', postRoutes)
+dotenv.config();
 
 app.use(bodyParser.json({limit: "30mb", extended:true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended:true}));
 app.use(cors());
 
+app.use('/posts', postRoutes)
 //credentials MongoDB Atlas
-const CONNECTION_URL = credentials.connection;
+const CONNECTION_URL = process.env.MONGODB_URL;
 const PORT = process.env.PORT || 5000;
 
 //connecting to MongoDB Atlas
