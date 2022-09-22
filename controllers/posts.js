ac2be917e6562fec9855a87d8e506dce78ae3873
NethZ <bynethz@gmail.com>
2022-09-21 22:38:20 -0500

    Adding query for posts by search

--- controllers/posts.js
+++ controllers/posts.js
@@ -11,6 +11,21 @@ export const getPosts = async (req, res) =>{
     }
 }
 
+export const getPostsBySearch = async (req, res) =>{
+    const { searchQuery, tags } = req.query
+    try {
+        //convert to regular expression
+        const title = new RegExp(searchQuery, 'i');
+        
+        const posts = await PostMessage.find({ $or: [{title}, {tags: { $in: tags.split(',') }}] })
+
+        res.json({ data: posts });
+        
+    } catch (error) {
+        res.status(404).json({ message: error.message })
+    }
+}
+
 //createdAt: new Date().toISOString getting error 409 Conflict with MongoDB
 
 export const createPosts = async (req, res) =>{
